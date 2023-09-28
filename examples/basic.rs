552ef82a3ae8f4d34a7c96bb62ef03f4d3e770bb
Jet_29 <jet_29@github.jet.hopto.me>
2023-09-28 23:49:34 +1000

    Starting off basic

---
+++ examples/basic.rs
@@ -0,0 +1,5 @@
+use spyder::run;
+
+fn main() {
+    run();
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,75 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::ffi::CString;
+
+use ash::vk;
+
+pub fn run() {
+    let entry: ash::Entry = ash::Entry::linked();
+
+    let engine_name = CString::new("Spyder").unwrap();
+    let app_name = CString::new("Example").unwrap();
+
+    let app_info = vk::ApplicationInfo::builder()
+        .application_name(&app_name)
+        .engine_name(&engine_name)
+        .engine_version(vk::make_api_version(0, 0, 1, 0))
+        .application_version(vk::make_api_version(0, 0, 1, 0))
+        .api_version(vk::API_VERSION_1_3);
+
+    let layer_names: Vec<CString> = vec![CString::new("VK_LAYER_KHRONOS_validation").unwrap()];
+
+    let layer_name_pointers: Vec<*const i8> = layer_names
+        .iter()
+        .map(|layer_name| layer_name.as_ptr())
+        .collect();
+
+    let extension_name_pointers: Vec<*const i8> =
+        vec![ash::extensions::ext::DebugUtils::name().as_ptr()];
+
+    let instance_create_info = vk::InstanceCreateInfo::builder()
+        .application_info(&app_info)
+        .enabled_extension_names(&layer_name_pointers)
+        .enabled_extension_names(&extension_name_pointers);
+
+    let instance: ash::Instance = unsafe {
+        entry
+            .create_instance(&instance_create_info, None)
+            .expect("Instance creation error")
+    };
+
+    let debug_utils = ash::extensions::ext::DebugUtils::new(&entry, &instance);
+    let debug_create_info = vk::DebugUtilsMessengerCreateInfoEXT {
+        message_severity: vk::DebugUtilsMessageSeverityFlagsEXT::WARNING
+            | vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE
+            | vk::DebugUtilsMessageSeverityFlagsEXT::INFO
+            | vk::DebugUtilsMessageSeverityFlagsEXT::ERROR,
+        message_type: vk::DebugUtilsMessageTypeFlagsEXT::GENERAL
+            | vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE
+            | vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION,
+        pfn_user_callback: Some(vulkan_debug_callback),
+        ..Default::default()
+    };
+
+    let utils_messenger = unsafe {
+        debug_utils
+            .create_debug_utils_messenger(&debug_create_info, None)
+            .expect("Failed to initialize debug messenger")
+    };
+
+    unsafe {
+        debug_utils.destroy_debug_utils_messenger(utils_messenger, None);
+        instance.destroy_instance(None)
+    };
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+unsafe extern "system" fn vulkan_debug_callback(
+    message_severity: vk::DebugUtilsMessageSeverityFlagsEXT,
+    message_type: vk::DebugUtilsMessageTypeFlagsEXT,
+    p_callback_data: *const vk::DebugUtilsMessengerCallbackDataEXT,
+    _user_data: *mut std::os::raw::c_void,
+) -> vk::Bool32 {
+    let message = std::ffi::CStr::from_ptr((*p_callback_data).p_message);
+    let severity = format!("{:?}", message_severity).to_lowercase();
+    let ty = format!("{:?}", message_type).to_lowercase();
+    println!("[Debug][{}][{}] {:?}", severity, ty, message); // TODO: Logging...
+    vk::FALSE
 }
