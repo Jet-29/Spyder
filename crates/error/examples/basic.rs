2b34b818526b2b7d320a0979e60a579219112d83
Jet_29 <github@jetgames.space>
2024-03-27 11:44:41 +1100

    Cleaned up error type. It is now a single struct that houses the error id and the error msg.

--- crates/error/examples/basic.rs
+++ crates/error/examples/basic.rs
@@ -1,31 +1,18 @@
-use error::prelude::*;
+use error::*;
 
-struct MyError;
-
-impl Error for MyError {
-    fn message(&self) -> &str {
-        "This is my error"
-    }
+fn return_error() -> EngineResult<()> {
+    EngineError::new("ErrorLess", "No error".to_string()).as_result()
 }
 
-fn return_error() -> Result<()> {
-    Err(Box::new(MyError))
-}
-
-fn return_ok() -> Result<u32> {
-    Ok(7)
+fn return_ok() -> EngineResult<u32> {
+    engine_error!("ErrorID", "An error occured here {}", 7).as_result()
 }
 
 fn main() {
-    let error = return_error().err().unwrap();
-    match error {
-        MyError => todo!(),
-    }
-
     match return_error() {
         Ok(_) => println!("No error"),
         Err(e) => println!("Error: {}", e),
-    }
+    };
 
     match return_ok() {
         Ok(val) => println!("Ok: {}", val),
--- crates/error/src/lib.rs
+++ crates/error/src/lib.rs
@@ -1,19 +1,42 @@
-use std::fmt::{Display, Formatter};
+use std::fmt::Display;
 
-pub mod prelude {
-    pub use super::Error;
-    pub use super::Result;
+pub type EngineResult<T> = std::result::Result<T, EngineError>;
 
+pub struct EngineError {
+    error_id: &'static str,
+    error_msg: String,
 }
 
-pub type Result<T> = std::result::Result<T, Box<dyn Error + 'static>>;
-
-pub trait Error {
-    fn message(&self) -> &str;
-}
-
-impl Display for Box<dyn Error + 'static> {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.message())
+impl EngineError {
+    pub fn new(error_id: &'static str, error_msg: String) -> Self {
+        Self {
+            error_id,
+            error_msg,
+        }
     }
+
+    pub fn as_result<T>(self) -> EngineResult<T> {
+        Err(self)
+    }
+
+    pub fn get_id(&self) -> &'static str {
+        self.error_id
+    }
+
+    pub fn get_msg(&self) -> &str {
+        &self.error_msg
+    }
+}
+
+impl Display for EngineError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Error ID: {} \t {}", self.error_id, self.error_msg)
+    }
+}
+
+#[macro_export]
+macro_rules! engine_error {
+    ($id:expr, $($args:tt)*) => {
+        $crate::EngineError::new($id, format!($($args)*))
+    };
 }
